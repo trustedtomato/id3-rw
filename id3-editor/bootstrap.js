8e3cb4347490855b92c4e1bbaba195d80dcdda08
trustedtomato <trustedtomato@users.noreply.github.com>
2021-01-11 17:50:37 +0000

    deploy: 2f220da1202f50e90687ccf7dd0f70a5b9f120a0

--- id3-editor/bootstrap.js
+++ id3-editor/bootstrap.js
@@ -386,7 +386,7 @@ eval("// A dependency graph that contains any wasm must all be imported\n// asyn
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"package_index_js":{"../package/wasm/id3_rw_bg.wasm":"a4adbf857e8302d8c65c"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"package_index_js":{"../package/wasm/id3_rw_bg.wasm":"e0f9239b14e50ad379dc"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
