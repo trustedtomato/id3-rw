7ab45863a5f234a46eacab4f29f05d5b035ed62c
trustedtomato <trustedtomato@users.noreply.github.com>
2021-01-12 16:12:04 +0000

    deploy: 16029f683a9ad9938445aba1aafb4863aa51fe54

---
+++ src_for-testing_index_js.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcreate_wasm_app"] = self["webpackChunkcreate_wasm_app"] || []).push([["src_for-testing_index_js"],{
+
+/***/ "./src/for-testing/index.js":
+/*!**********************************!*\
+  !*** ./src/for-testing/index.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var id3_rw__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! id3-rw */ \"../package/index.js\");\n\n\nconst readyElement = document.getElementById('ready')\n\nwindow.id3 = id3_rw__WEBPACK_IMPORTED_MODULE_0__\nreadyElement.style.display = 'block'\n\n\n//# sourceURL=webpack://create-wasm-app/./src/for-testing/index.js?");
+
+/***/ })
+
+}]);
